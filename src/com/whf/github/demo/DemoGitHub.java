a2088080f283e3b498e2f611a679cf4f0b805b50
chenfengbaobao <15538323378@163.com>
2018-07-08 22:10:05 +0800

    add syso

--- src/com/whf/github/demo/DemoGitHub.java
+++ src/com/whf/github/demo/DemoGitHub.java
@@ -3,7 +3,7 @@ package com.whf.github.demo;
 public class DemoGitHub {
 
     public static void main(String[] args) {
-
+        System.out.println("============whf====begin=================");
 
         for (int i = 0; i <20 ; i++) {
             System.out.println("wuhaifneg love github ---->>>>"+i);
@@ -14,5 +14,7 @@ public class DemoGitHub {
         for (int i = 0; i <5 ; i++) {
             System.out.println("wuhaifneg love github ---->>>>"+i);
         }
+        System.out.println("============whf========end=============");
+
     }
 }
