5a041605698ea170ab0875109079956fd4382499
chenfengbaobao <15538323378@163.com>
2018-07-08 22:01:42 +0800

    Initial commit

---
+++ src/com/whf/github/demo/DemoGitHub.java
@@ -0,0 +1,12 @@
+package com.whf.github.demo;
+
+public class DemoGitHub {
+
+    public static void main(String[] args) {
+
+
+        for (int i = 0; i <20 ; i++) {
+            System.out.println("wuhaifneg love github ---->>>>"+i);
+        }
+    }
+}
